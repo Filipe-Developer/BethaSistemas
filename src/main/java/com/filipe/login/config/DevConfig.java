31996006af08423a53d635367e2fa9158ec59875
Filipe55555 <filpe55555@gmail.com>
2021-07-06 18:10:11 -0300

    Implementação BD

--- src/main/java/com/filipe/login/config/DevConfig.java
+++ src/main/java/com/filipe/login/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.filipe.login.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,14 +9,20 @@ import org.springframework.context.annotation.Profile;
 import com.filipe.login.services.DBservice;
 
 @Configuration
-@Profile("test")
-public class testConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
-	private DBservice dbservice;
+	private DBservice dBservice;
+	
+	@Value("$(spring.jpa.hibernate.ddl-auto)")
+	private String strategy;
 	
 	@Bean
-	public void instanciaDB() {
-		this.dbservice.instanciaDB();
+	public boolean instanciaDB() {
+		if(strategy.equals("create")) {
+			this.dBservice.instanciaDB();
+		}
+		return false;
 	}
 }
